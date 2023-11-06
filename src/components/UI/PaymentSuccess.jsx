1d28545f203cc8ce3ff590be95e21212344e6f47
Dhruv Rajpoot <dhruv.kutarya@gmail.com>
2023-11-06 13:30:48 +0530

    feat: useaxios N details on payment success page

--- src/components/UI/PaymentSuccess.jsx
+++ src/components/UI/PaymentSuccess.jsx
@@ -1,18 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import "../../styles/payment-success.css";
+import { SERVER_URL } from "../../config/config.jsx";
+import useaxios from "../../utils/useaxios.jsx";
+import axios from "axios";
 
 export const PaymentSuccess = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const refrenceId = searchParams.get("reference_id");
+  const paymentId = searchParams.get("payment_id");
+  const orderId = searchParams.get("order_id");
+  const [loading, setLoading] = useState(true);
+  const [orderDetails, setOrderDetails] = useState({});
+
+  const fetchOrderDetails = async () => {
+    const api = useaxios();
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `${SERVER_URL}/order/getorder/${orderId}`
+      );
+      if (response.status === 200) {
+        console.log(response.data);
+        setOrderDetails(response.data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchOrderDetails();
+  }, []);
 
   return (
     <div className="payment-success">
-      <h3 className="payment-success-heading">
-        Your rental car is successfully booked
-      </h3>
-      <h5>Thank you for choosing our service</h5>
+      {loading ? (
+        <div className="loader">
+          <div className="loader-spinner">Loading...</div>
+        </div>
+      ) : (
+        <>
+          {Object.keys(orderDetails).length === 0 ? (
+            <div>Order not found</div>
+          ) : (
+            <div>
+              <h3 className="payment-success-heading">
+                Your rental car is successfully booked
+              </h3>
+              <h5>Thank you for choosing our service</h5>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 };
